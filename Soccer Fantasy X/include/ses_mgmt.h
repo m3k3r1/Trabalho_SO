5c82c30fb13b9350c24e3f87f2a54f75b916964a
m3k3r1 <m3k3r1@gmail.com>
2017-01-02 20:37:25 +0000

    player position initilize

--- Soccer Fantasy X/include/ses_mgmt.h
+++ Soccer Fantasy X/include/ses_mgmt.h
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include "stdbool.h"
 
 typedef struct game_stat_t game_stat_t;
  struct game_stat_t{
@@ -17,9 +18,8 @@ typedef struct {
     int player;
     int pos_x;
     int pos_y;
-    //bool have_ball;
+    bool have_ball;
 } game_control_t;
-
 
 void set_game(game_stat_t** head, int t);
 game_stat_t* new_game(int t);
--- Soccer Fantasy X/src/usr_ui.c
+++ Soccer Fantasy X/src/usr_ui.c
@@ -17,7 +17,6 @@ void init_win_params(WIN *p_win){
 	p_win->border.bottom_l = '+';
 	p_win->border.bottom_r = '+';
 }
-
 void create_box(WIN *p_win){
 	int x, y, w, h, m_x, m_y;
 	chtype middle_line = '|';
@@ -33,12 +32,48 @@ void create_box(WIN *p_win){
 	mvaddch(y, x + w, p_win->border.top_r);
 	mvaddch(y + h, x, p_win->border.bottom_l);
 	mvaddch(y + h, x + w, p_win->border.bottom_r);
-
 	mvvline(m_y + 1, m_x, middle_line, h-1);
-
 	mvhline(y, x + 1, p_win->border.top_s, w - 1);
 	mvhline(y + h, x + 1, p_win->border.bottom_s, w - 1);
-	mvvline(y + 1, x, p_win->border.left_s, h - 1);
+	mvvline(y + 1, x, p_win->border.left_s, h-1);
 	mvvline(y + 1, x + w, p_win->border.right_s, h - 1);
+
 	refresh();
 }
+void init_players(WIN* p_win){
+	chtype goal_keeper = '0';
+	chtype defense = '1';
+	chtype attack = '2';
+
+	int goal_keeper_x = p_win->startx + 7*(p_win->width/8);
+	int goal_keeper_y = p_win->starty + p_win->height/2;
+	int defense_x =   p_win->startx + 3*(p_win->width/4);
+	int defense_y = p_win->starty + p_win->height/4;
+	int attack_x = p_win->startx +  5*((p_win->width/2)/4);
+	int attack_y =  p_win->starty + p_win->height/4;
+
+	mvaddch(goal_keeper_y, goal_keeper_x + 2 ,goal_keeper);
+	mvaddch(defense_y, defense_x, defense);
+	mvaddch(defense_y + 10.5, defense_x, defense);
+	mvaddch(attack_y, attack_x, attack);
+	mvaddch(attack_y + 5.25, attack_x, attack);
+	mvaddch(attack_y + 10.5, attack_x, attack);
+	refresh();
+}
+void init_fld(WIN* p_win){
+    initscr();
+    start_color();
+    cbreak();
+    keypad(stdscr, TRUE);
+    noecho();
+    init_pair(1, COLOR_CYAN, COLOR_BLACK);
+    init_win_params(p_win);
+    attron(COLOR_PAIR(1));
+    printw("Press F1 to exit");
+    refresh();
+    attroff(COLOR_PAIR(1));
+    //create_box(p_win);
+    //init_players(p_win);
+
+
+};
