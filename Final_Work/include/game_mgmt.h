85a75907eea6db74b3707be4cb266a30c1337f2c
g4rrucho <guilhermegarrucho@gmail.com>
2017-01-07 18:50:12 +0000

    Game handling and setup done

--- Final_Work/include/game_mgmt.h
+++ Final_Work/include/game_mgmt.h
@@ -9,6 +9,7 @@ struct player_t
 {
   int role, id;             // PLAYER ROLE AND ID
   int posX, posY;           // PLAYER POS
+  int run;              // GAME SECONDS
   pthread_t tid;            // PLAYER MOVEMENT THREAD
   player_t * next, * head;  // NEXT PLAYER PTR
 };
@@ -16,13 +17,22 @@ struct player_t
 // GAME STRUCT
 typedef struct
 {
-  int seconds;      // GAME SECONDS
-  int res[2];       // GAME RESULT
-  player_t * list;  // GAME PLAYER LIST
-  pthread_t tid;    // TID FOR GAME FUNCTION
+  int seconds;        // GAME SECONDS
+  int res[2];         // GAME RESULT
+  int numPlayers;     // GAME PLAYER NUM
+  player_t * p_list;  // GAME PLAYER LIST
+  pthread_t tid;      // TID FOR GAME FUNCTION
 } game_t;
 
 // TODO PLAYER STRUCT NEEDED FOR CLIENT?! SO CLIENT DOESN'T HAVE ACCESS TO ALL INFO
 
-// THREAD FUNCTION FOR GAME CONTROL
-void * runGame(void * arg);
+
+//SETUP GAME FUNCTIONS
+//void getNumPlayers(int * numDef, int * numOff); // GET GAME PLAYER NUM
+//void initPlayers(game_t * game, int numDef, int numOff); // INIT PLAYER DATA
+void set_game(game_t * game, int sec); // SETUP REQUIRED DATA FOR GAME
+
+
+// HANDLING GAME FUNCTIONS
+void * playerMovement(void * arg);
+void * runGame(void * arg); // THREAD FUNCTION FOR GAME CONTROL
