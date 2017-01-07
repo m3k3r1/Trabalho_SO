b26ab359a0c788b77d697c8a865994880dc54653
g4rrucho <guilhermegarrucho@gmail.com>
2017-01-07 15:37:41 +0000

    Game & player struct prototype.

--- Final_Work/include/game_mgmt.h
+++ Final_Work/include/game_mgmt.h
@@ -1,9 +1,28 @@
 #include "base.h"
 
+#define WIDTH 51
+#define HEIGHT 21
+
+// GAME PLAYER STRUCT
+typedef struct player_t player_t;
+struct player_t
+{
+  int role, id;             // PLAYER ROLE AND ID
+  int posX, posY;           // PLAYER POS
+  pthread_t tid;            // PLAYER MOVEMENT THREAD
+  player_t * next, * head;  // NEXT PLAYER PTR
+};
+
+// GAME STRUCT
 typedef struct
 {
-  int seconds;
-  pthread_t tid;
+  int seconds;      // GAME SECONDS
+  int res[2];       // GAME RESULT
+  player_t * list;  // GAME PLAYER LIST
+  pthread_t tid;    // TID FOR GAME FUNCTION
 } game_t;
 
+// TODO PLAYER STRUCT NEEDED FOR CLIENT?! SO CLIENT DOESN'T HAVE ACCESS TO ALL INFO
+
+// THREAD FUNCTION FOR GAME CONTROL
 void * runGame(void * arg);
--- Final_Work/src/game_mgmt.c
+++ Final_Work/src/game_mgmt.c
@@ -1,15 +1,30 @@
 #include "game_mgmt.h"
 
+// INITIALIZE RAND NUM SEQUENCE, OTHERWISE THE SEQUENCE IS ALWAYS THE SAME
+void init_random_gen()
+{
+  srand((unsigned) time(NULL));
+}
+
+// GENERATES RANDOM NUM BETWEEN MIN AND MAX, INCLUDING
+int randNum(int min, int max)
+{
+  return min + rand() % (max - min + 1);
+}
+
+// THREAD FUNCTION FOR GAME CONTROL
 void * runGame(void * arg)
 {
   int i = 0;
   game_t * game = (game_t *) arg;
 
+  // GAME RUNS WHILE COUNTER HASN'T REACH GAME SECONDS
   while(i < game->seconds)
   {
     sleep(1);
     i++;
   }
+
   // TERMINATE ALL THREADS AND USERS
   // SHOW GOALS
 
